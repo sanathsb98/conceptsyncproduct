0c70224154724a01c2a1ae2c221fbc67e1bfb3bc
sanathsb98 <itssanathsb@gmail.com>
2025-06-23 12:22:07 +0530

    commit

---
+++ config.js
@@ -0,0 +1,6 @@
+window.env={
+    FRONTEND_URL: "https://conceptsyncproduct.vercel.app",
+    BACKEND_URL: "https://conceptsyncbackend.vercel.app",
+}
+// "https://conceptsyncproduct.vercel.app"
+// "http://localhost:3000"
